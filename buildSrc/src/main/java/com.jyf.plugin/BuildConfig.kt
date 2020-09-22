e562114eea156603c7e108898747891e6f95dce7
贾元发 <jiayuanfa@yeah.net>
2020-09-22 11:22:57 +0800

    feat: 设置icon，增加Hilt注入模块

---
+++ buildSrc/src/main/java/com.jyf.plugin/BuildConfig.kt
@@ -0,0 +1,10 @@
+package com.jyf.plugin
+
+object BuildConfig {
+    const val compileSdkVersion = 29
+    const val buildToolsVersion = "29.0.3"
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 29
+    const val versionCode = 10000
+    const val versionName = "1.0.0"
+}
---
+++ buildSrc/src/main/java/com.jyf.plugin/DependencyManager.kt
@@ -0,0 +1,13 @@
+package com.jyf.plugin
+
+object Versions {
+    const val hit = "2.28-alpha"
+    const val hitViewModule = "1.0.0-alpha01"
+}
+
+object Hilt {
+    const val daggerRuntime = "com.google.dagger:hilt-android:${Versions.hit}"
+    const val daggerCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hit}"
+    const val viewModule = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.hitViewModule}"
+    const val compiler = "androidx.hilt:hilt-compiler:${Versions.hitViewModule}"
+}
